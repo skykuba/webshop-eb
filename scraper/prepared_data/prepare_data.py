1b18be3f014715c4870d6dac48afb7507e2518d3
Igor <s197954@student.pg.edu.pl>
2025-12-03 17:42:32 +0100

    Change name of path and implement load_products()

--- scraper/prepared_data/prepare_data.py
+++ scraper/prepared_data/prepare_data.py
@@ -1,5 +1,5 @@
 # TODO take choosen products from data/products.json and make a new valid for prestashop json file
-# TODO save two photos per product (if exist) in prepared_data/images/{photo_name}
+# TODO save two photos per product (if exist) in prepared_data/photos/{photo_name}
 # TODO for sized products (shoes) make combinations.json files with available sizes
 import json
 import os
@@ -11,10 +11,13 @@ WANDTED_SUBATEGORIES: List[str] = []
 abspath: str = os.path.dirname(os.path.abspath(__file__))
 input_data_file: str = os.path.join(abspath, "../data/products.json")
 output_data_file: str = os.path.join(abspath, "prepared_products.json")
-images_output_dir: str = os.path.join(abspath, "images")
+photos_output_dir: str = os.path.join(abspath, "photos")
 
-# def load_products(products_file: str) -> List[Dict[str, Any]]:
-'''Load products from a JSON file.'''
+def load_products(products_file: str) -> List[Dict[str, Any]]:
+    '''Load products from a JSON file.'''
+    with open(products_file, 'r', encoding='utf-8') as f:
+        products: List[Dict[str, Any]] = json.load(f)
+    return products
 
 # def filter_products(products: List[Dict[str, Any]], WANTED_SUBCATEGORIES: List[str]) -> List[Dict[str, Any]]:
 '''Filter products based on wanted subcategories.'''
@@ -25,8 +28,8 @@ images_output_dir: str = os.path.join(abspath, "images")
 # def save_prepared_data(prepared_products: List[Dict[str, Any]], output_data_file: str) -> None:
 '''Save prepared product data to a JSON file.'''
 
-# def save_product_images(product: Dict[str, Any], images_dir: str) -> None:
-'''Save product images to a specified directory.'''
+# def save_product_photos(product: Dict[str, Any], photos_dir: str) -> None:
+'''Save product photos to a specified directory.'''
 
 # def generate_combinations(product: Dict[str, Any]) -> List[Dict[str, Any]]:
 '''Generate size combinations for sized products.'''
