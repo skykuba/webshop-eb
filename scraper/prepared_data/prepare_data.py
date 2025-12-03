fb32b848be1846f9413b09ede285ecdbcb96abd7
Igor <s197954@student.pg.edu.pl>
2025-12-03 18:47:14 +0100

    Add load_categories function and update prepare_product_data structure

--- scraper/prepared_data/prepare_data.py
+++ scraper/prepared_data/prepare_data.py
@@ -1,6 +1,8 @@
+# TODO POST used categories to prestashop to get category ids
 # TODO take choosen products from data/products.json and make a new valid for prestashop json file
 # TODO save two photos per product (if exist) in prepared_data/photos/{photo_name}
 # TODO for sized products (shoes) make combinations.json files with available sizes
+
 import json
 import os
 from typing import Any, Dict, List
@@ -24,6 +26,7 @@ SIZE_CATEGORIES: List[str] = ["buty"]
 
 # File paths
 abspath: str = os.path.dirname(os.path.abspath(__file__))
+categories_file: str = os.path.join(abspath, "../data/categories.json")
 input_data_file: str = os.path.join(abspath, "../data/products.json")
 output_data_file: str = os.path.join(abspath, "prepared_products.json")
 photos_output_dir: str = os.path.join(abspath, "photos")
@@ -34,6 +37,15 @@ def load_products(products_file: str) -> List[Dict[str, Any]]:
         products: List[Dict[str, Any]] = json.load(f)
     return products
 
+def load_categories(categories_file: str) -> Dict[str, Any]:
+    '''Load categories from a JSON file.'''
+    with open(categories_file, 'r', encoding='utf-8') as f:
+        categories: Dict[str, Any] = json.load(f)
+    return categories
+
+# def post_categories(categories: Dict[str, Any]) -> Dict[str, int]:
+    '''Post categories to Prestashop and return a mapping of category names to their Prestashop IDs.'''
+
 def filter_products(products: List[Dict[str, Any]], WANTED_SUBCATEGORIES: List[str]) -> List[Dict[str, Any]]:
     '''Filter products based on wanted subcategories.'''
     filtered_products: List[Dict[str, Any]] = [
@@ -41,9 +53,50 @@ def filter_products(products: List[Dict[str, Any]], WANTED_SUBCATEGORIES: List[s
     ]
     return filtered_products
 
-# def prepare_product_data(product: Dict[str, Any]) -> Dict[str, Any]:
+def prepare_product_data(product: Dict[str, Any]) -> Dict[str, Any]:
     '''Prepare product data for Prestashop format.'''
-
+    """
+    "product": {
+        "active": 1,
+        "id_category_default": subcategory_id, # have use GET /api/categories to get category ids
+        "price": price, # netto price
+        "reference": product['parameters'].get('Kod EAN', ''),
+        "ean13": product['parameters'].get('Kod EAN', ''),
+        "name": {
+            "language": [
+                {
+                    "id": 1,
+                    "value": product['name']
+                }
+            ]},
+        "description": {
+            "language": [
+                {
+                    "id": 1,
+                    "value": product['long_description']
+                }
+            ]},
+        "description_short": {
+            "language": [
+                {
+                    "id": 1,
+                    "value": product['short_description']
+                }
+            ]},
+        "minimal_quantity": 1,
+        "id_tax_rules_group": 1,
+        "depends_on_stock": if_size, # 1 for size products
+        "out_of_stock": 0, # 0 - deny orders
+        "quantity": total_quantity, # if_size == 1 do not contain quantity
+        "associations": {
+            "categories": [
+                    {"id": root_id},
+                    {"id": subcategory_id},
+                    {"id": category_id}
+            ]
+    }
+    
+    """
 # def save_prepared_data(prepared_products: List[Dict[str, Any]], output_data_file: str) -> None:
     '''Save prepared product data to a JSON file.'''
 
