658ea15f7f91c93b031ed9f0e6b769bdf5fe1e84
skykuba <s198088@student.pg.edu.pl>
2025-12-10 20:20:38 +0100

    Add cahce on delivery and wire method with working invoice download

--- prestashop/src/src/PrestaShopBundle/Install/Install.php
+++ prestashop/src/src/PrestaShopBundle/Install/Install.php
@@ -924,6 +924,7 @@ class Install extends AbstractInstall
             'ps_shoppingcart',
             'ps_socialfollow',
             'ps_themecusto',
+            'ps_cashondelivery',
             'ps_wirepayment',
             'statsbestcategories',
             'statsbestcustomers',
